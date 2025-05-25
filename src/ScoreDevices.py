ee5282b97b5c05f8b426a63ebdcc9502bf19f77b
michael <michaelrantohi@gmail.com>
2025-05-25 10:13:18 -0400

    Cleaned up class variables and functions in order to be able to score multiple macs at once instead of creating separate instances

--- src/ScoreDevices.py
+++ src/ScoreDevices.py
@@ -3,11 +3,6 @@ from datetime import datetime
 
 class ScoreDevices:
     def __init__(self, mac):
-        self.trusted_vendors = [
-            "clevo",
-            "dyson",
-            "apple"
-        ]
 
         self.vendor_trust_table = {
             "TRUSTED VENDOR": 30,
@@ -34,38 +29,41 @@ class ScoreDevices:
         }
 
         self.parser = manuf.MacParser()
-        self.vendor_trust = ""
-        self.vendor_name = ""
-        self.mac = mac
 
     # Collects vendor manufacturer info
     def get_vendor(self, mac) -> str:
-        self.vendor_name = self.parser.get_manuf(mac) or "UNKNOWN"
-        return self.vendor_name
+        vendor_name = self.parser.get_manuf(mac) or "UNKNOWN"
+        return vendor_name
 
     # Checks to see if vendor is in list of trusted vendors
     def check_vendor_trust(self, vendor: str) -> str:
+        trusted_vendors = [
+            "clevo",
+            "dyson",
+            "apple"
+        ]
+
         normalized = vendor.strip().lower()
         if normalized == "unknown":
-            self.vendor_trust = "UNKNOWN"
-        elif normalized in self.trusted_vendors:
-            self.vendor_trust = "TRUSTED VENDOR"
+            vendor_trust = "UNKNOWN"
+        elif normalized in trusted_vendors:
+            vendor_trust = "TRUSTED VENDOR"
         else:
-            self.vendor_trust = "UNTRUSTED VENDOR"
-        return self.vendor_trust
+            vendor_trust = "UNTRUSTED VENDOR"
+        return vendor_trust
 
     # Creates a score based on a variety of factors
-    def score_device(self) -> int:
+    def score_device(self, mac) -> int:
         score = 0
 
-        vendor = self.get_vendor(self.mac)
+        vendor = self.get_vendor(mac)
         ven_trust = self.check_vendor_trust(vendor)
         score += self.vendor_trust_table.get(ven_trust, 0)
 
-        ven_type = self.check_vendor_classifier()
+        ven_type = self.check_vendor_classifier(vendor)
         score += self.vendor_type_table.get(ven_type, 0)
 
-        mac_type = self.check_mac_type()
+        mac_type = self.check_mac_type(mac)
         score += self.mac_type_table.get(mac_type, 0)
 
         cx_time = self.check_connection_time()
@@ -80,24 +78,24 @@ class ScoreDevices:
         work_end = now.replace(hour=19, minute=0, second=0, microsecond=0)
 
         if now < work_start or now > work_end:
-            return "off-hours"
+            return "off_hours"
         else:
-            return "on-hours"
+            return "on_hours"
 
     # Scores vendor based on classifiers that may be untrustworthy or trustworthy
-    def check_vendor_classifier(self):
-        if "camera" in self.vendor_name:
+    def check_vendor_classifier(self, vendor_name):
+        if "camera" in vendor_name:
             return "camera"
-        elif "laptop" in self.vendor_name:
+        elif "laptop" in vendor_name:
             return "laptop"
-        elif "intel" in self.vendor_name:
+        elif "intel" in vendor_name:
             return "intel"
         else:
             return ""
 
     # Checks for mac spoofing
-    def check_mac_type(self):
-        if self.mac.lower() in ["00:00:00:00:00:00", "ff:ff:ff:ff:ff:ff"] or int(self.mac.split(':')[0], 16) & 2:
+    def check_mac_type(self, mac):
+        if mac.lower() in ["00:00:00:00:00:00", "ff:ff:ff:ff:ff:ff"] or int(mac.split(':')[0], 16) & 2:
             return "sketchy_mac"
         else:
             return "okay_mac"
