b5f720b6b075c70f1bb0f39beb3b9f236bedc88e
michael <michaelrantohi@gmail.com>
2025-09-19 11:14:09 -0400

    Added menu option for virus total, still testing out output

--- src/VirusTotalAPI.py
+++ src/VirusTotalAPI.py
@@ -2,8 +2,13 @@ import requests
 
 class VirusTotalAPI:
     def __init__(self):
-        self.headers = {"accept": "application/json"}
+        self.headers = {
+            "accept": "application/json",
+            "x-apikey": "APIKEY"
+        }
+        self.ip = "1.1.1.1"
 
-    def get_url(self,ip ):
-        ip = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
-        return requests.get(ip, headers=self.headers)
+    def get_url(self):
+        url = f"https://www.virustotal.com/api/v3/ip_addresses/{self.ip}"
+        response = requests.get(url, headers=self.headers)
+        return response.text
